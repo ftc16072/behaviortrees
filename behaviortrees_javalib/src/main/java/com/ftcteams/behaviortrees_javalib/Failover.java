d1aefbeb88fbb0ed887fa2e47990d0b3ca7f41e3
Alan Smith <alan@randomsmiths.com>
2024-06-19 17:02:44 -0400

    Remove everything except library

---
+++ behaviortrees_javalib/src/main/java/com/ftcteams/behaviortrees_javalib/Failover.java
@@ -0,0 +1,29 @@
+package com.ftcteams.behaviortrees_javalib;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Failover extends Node {
+    List<Node> children;
+
+    public Failover(Node ... a) {
+        this.children = Arrays.asList(a);
+    }
+
+    @Override
+    public State tick(DebugTree debug, Object obj) {
+        debug.startParent(this);
+        for (Node child : children) {
+            debug.addNode(child);
+            State state = child.tick(debug, obj);
+            debug.updateNode(child, state);
+
+            if (state == State.SUCCESS) {
+                return State.SUCCESS;
+            } else if (state == State.RUNNING) {
+                return State.RUNNING;
+            }
+        }
+        return State.FAILURE;
+    }
+}
